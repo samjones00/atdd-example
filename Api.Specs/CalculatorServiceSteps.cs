d873038d5fa16d07cdd6971301ea50a01d40587f
sam@smjdesign.co.uk <sam@smjdesign.co.uk>
2020-11-27 00:25:04 +0000

    Adding specflow scenarios and calculator code

---
+++ Api.Specs/CalculatorServiceSteps.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace App.Specs
+{
+    [Binding]
+    public class CalculatorServiceSteps
+    {
+        private readonly CalculatorService _calculatorService;
+
+        public CalculatorServiceSteps()
+        {
+            _calculatorService = new CalculatorService();
+        }
+
+        [Given(@"the first number of (.*) is added")]
+        public void GivenTheFirstNumberIs(decimal p0)
+        {
+            _calculatorService.Add(p0);
+        }
+        
+        [Given(@"the second number of (.*) is added")]
+        public void GivenTheSecondNumberIs(decimal p0)
+        {
+            _calculatorService.Add(p0);
+        }
+
+        [Given(@"the third number of (.*) is subtracted")]
+        public void GivenTheThirdNumberIs(decimal p0)
+        {
+            _calculatorService.Subtract(p0);
+        }
+        
+        [When(@"the numbers are calculated")]
+        public void WhenTheTwoNumbersAreAdded()
+        { 
+            _calculatorService.Calculate();
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(decimal p0)
+        {
+            _calculatorService.Total().Should().Be(p0);
+        }
+    }
+}
---
+++ App/CalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace App
+{
+    public class CalculatorService
+    {
+        private decimal _total;
+        private readonly IList<decimal> _actions = new List<decimal>();
+
+        public void Add(decimal value) => _actions.Add(value);
+        public void Subtract(decimal value) => _actions.Add(value * -1);
+        public void Calculate() => _total = _actions.Sum();
+        public decimal Total()=>_total;
+    }
+}
